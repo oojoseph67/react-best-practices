03d6513d3ba0c66ba4123de4fc0e05eb1114f8cb
Obinna Okolo <oojoseph67@gmail.com>
2023-09-24 14:12:51 +0100

    relocate state (form)

--- rightCode.js
+++ rightCode.js
@@ -1,5 +1,18 @@
-const refElement = React.useCallback((node) => {
-  if (node !== null) {
-    setHeight(node.getBoundingClientReact().height);
-  }
-}, []);
+export default function App() {
+  return (
+    <>
+      <Form />
+      <PageContent />
+    </>
+  );
+}
+
+function Form() {
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <form>
+      <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+    </form>
+  );
+}
--- wrongCode.js
+++ wrongCode.js
@@ -1,5 +1,15 @@
-React.useEffect(() => {
-  if (refElement.current) {
-    setHeight(refElement.current.offsetHeight);
-  }
-}, [refElement.current]);
+export default function App() {
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <>
+      <form>
+        <input
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </form>
+      <PageContent />
+    </>
+  );
+}
