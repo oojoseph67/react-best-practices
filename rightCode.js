359b779cc8ac778b8f958188661f9b084ad78fb5
Obinna Okolo <oojoseph67@gmail.com>
2023-09-24 14:07:57 +0100

    refChanges

--- rightCode.js
+++ rightCode.js
@@ -1,15 +1,5 @@
-const App = () => {
-  const users = {
-    ids: ["1", "2", "3"],
-    entities: {
-      1: { id: 1, name: "george" },
-      2: { id: 2, name: "susan" },
-      3: { id: 3, name: "bob" },
-    },
-  };
-  const findUserById = (id) => {
-    return users.find((user) => user.id === id);
-  };
-
-  return <UsersList users={users} />;
-};
+const refElement = React.useCallback((node) => {
+  if (node !== null) {
+    setHeight(node.getBoundingClientReact().height);
+  }
+}, []);
--- wrongCode.js
+++ wrongCode.js
@@ -1,13 +1,5 @@
-const App = () => {
-  const users = [
-    { id: 1, name: "george" },
-    { id: 2, name: "susan" },
-    { id: 3, name: "bob" },
-  ];
-
-  const findUserById = (id) => {
-    return users.find((user) => user.id === id);
-  };
-
-  return <UsersList users={users} />;
-};
+React.useEffect(() => {
+  if (refElement.current) {
+    setHeight(refElement.current.offsetHeight);
+  }
+}, [refElement.current]);
