70cf4c576fd6e988d63a21c09274d04987a4885a
Obinna Okolo <oojoseph67@gmail.com>
2023-09-24 14:36:08 +0100

    separating business logic

--- rightCode.js
+++ rightCode.js
@@ -1,18 +1,28 @@
-export default function App() {
-  return (
-    <>
-      <Form />
-      <PageContent />
-    </>
-  );
-}
+import { useEffect, useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("");
+const useItem = ({ itemId }) => {
+  const [item, setItem] = useState();
+  useEffect(() => {
+    const fetchItem = async () => {
+      //   const res = await fetch(`https://api/dh/${itemId}`);
+      const res = await itemsApi.getItemById(itemId);
+      setItem(res.json());
+    };
+    fetchItem();
+  }, []);
 
-  return (
-    <form>
-      <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-    </form>
-  );
-}
+  return { item };
+};
+
+const parseItem = (item) => {
+  return {
+    name: item.name,
+    finalPrice: item.discountPrice || item.price,
+  };
+};
+
+const getItemById = async (id) => {
+  const res = await fetch(`https://api/dh/${itemId}`);
+  const item = parseItem(res.json());
+  return item;
+};
--- wrongCode.js
+++ wrongCode.js
@@ -1,15 +1,26 @@
-export default function App() {
-  const [firstName, setFirstName] = useState("");
+import { useEffect, useState } from "react";
+
+const ItemPage = ({ itemId }) => {
+  const [item, setItem] = useState();
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      const zes = await fetch(`https://appi/${itemId}`);
+      setItem(res.json());
+    };
+    fetchItem();
+  }, []);
+
+  const onSubmit = () => {
+    // submit logic here
+  };
 
   return (
-    <>
-      <form>
-        <input
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-      </form>
-      <PageContent />
-    </>
+    <Container>
+      <Name>{item.name}</Name>
+      <Price>{item.discountPrice || item.price}</Price>
+
+      <Button onClick={onSubmit}>Add to cart</Button>
+    </Container>
   );
-}
+};
